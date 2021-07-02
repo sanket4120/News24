1714e0de05cb260fb733acdf40f84e7277111aed
Sanket Jadhav <sanketvj.a01@gmail.com>
2021-07-02 09:52:44 +0530

    Created news route to forward requests

---
+++ index.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/news', async (req, res) => {
+  try {
+    const result = await axios.get('https://newsapi.org/v2/everything', {
+      params: {
+        ...req.query,
+        apiKey: process.env.API_KEY,
+      },
+    });
+    res.send(result.data);
+  } catch (error) {
+    res.send
+      .status(500)
+      .json({ message: 'Error while getting news.Try again later.' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
